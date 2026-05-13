ba78ba22ad5a9452e063747f8db2f6ace6a5c3dd
Glen Sihotang <gleensihotang@gmail.com>
2026-05-13 20:46:51 +0700

    add feature create file and menu

--- To Do List.py
+++ To Do List.py
@@ -1,10 +1,20 @@
-a = "[ ] tugas b.indo"
+def create_file():
+    with open("To Do List.txt", "w") as file:
+        file.write("To Do List:\n")
+    print("To Do List file created successfully.\n")
 
-b = "[ ]" in a
 
-print(b)
-print()
-if b == True:
-    a = a.replace("[ ]", "[\u2713]")
+while True:
+    print("=========================================")
+    print("Welcome to the To Do List Program")
+    print("1. Create To Do List")
+    print("2. Exit")
+    choice = int(input("Enter your choice: "))
 
-print(a)
+    if choice == 1:
+        create_file()
+    elif choice == 2:
+        break
+    else:
+        print("Invalid choice. Please try again.")
+        
